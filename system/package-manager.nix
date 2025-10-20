a47fdab7a83068fb54c0eaaf00ac09874b04b2ef
Urtyom-Alyanov <mc2017artemklochkcov435@yandex.ru>
2025-10-20 22:55:12 +0300

    add pipe-operators

--- system/package-manager.nix
+++ system/package-manager.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }:
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" "pipe-operators" ];
   nix.settings.auto-optimise-store = true;
   nix.gc = {
     automatic = true;
